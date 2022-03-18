3671bf3a39bce164324acd8f51f3280df3058b50
Rafael Perger <rafael.perger@gmail.com>
2022-03-18 18:32:46 +0100

    Gropu module into module pattern. Add displayDate function for testin purpose.

--- src/dom.js
+++ src/dom.js
@@ -1,7 +1,15 @@
-class DOMmanipulation {
-    displayTask (task) {
+
+
+const DOMManipulation = (() => {
+    function displayTask (task) {
         console.log(task.title, task.description);
     }
-}
 
-export {DOMmanipulation};
+    function displayDate(task) {
+        console.log(`${task.title}: ${task.dueDate}`);
+    } 
+
+    return {displayDate, displayTask}
+})();
+
+export {DOMManipulation};
