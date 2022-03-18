ba3bed57da25d45137a8bf53ebdb275d796c86b9
Rafael Perger <rafael.perger@gmail.com>
2022-03-18 18:09:11 +0100

    Create task class and export it.

---
+++ src/task.js
@@ -0,0 +1,10 @@
+class Task {
+    constructor (title, description, dueDate, priotiry) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priotiry = priotiry;
+    }
+}
+
+export { Task };
