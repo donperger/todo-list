641c8da3b4d58cf7e60766bd125364687a2fdfef
Rafael Perger <rafael.perger@gmail.com>
2022-03-18 18:33:38 +0100

    Import DOMManipulation module pattern.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-import { Task } from "./task";
+import { DOMManipulation } from "./dom.js";
+import { Task } from "./task.js";
 
-let task = new Task("Wash dishes", "Clea all the dirty dishes in the sink", "2022.04.18", "not important");
+
+let task = new Task("Wash dishes", "Clean all the dirty dishes in the sink", "2022.04.18", "not important");
+
+DOMManipulation.displayTask(task);
+
+DOMManipulation.displayDate(task);
